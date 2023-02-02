93a5561399c9c4c472f4040a4ffdf2881548cb74
Daniil Kirilenko <dkirilenko@Daniils-MacBook-Air.local>
2023-02-02 13:20:18 +0500

    log dream rollout

--- dreamer.py
+++ dreamer.py
@@ -13,6 +13,7 @@ import torch.distributions as distributions
 
 from collections import OrderedDict
 
+from envs.atari import Atari
 import env_wrapper
 from replay_buffer import ReplayBuffer
 from models import RSSM, ConvEncoder, ConvDecoder, DenseDecoder, ActionDecoder
@@ -283,16 +284,30 @@ class Dreamer:
         episode_rew = np.zeros((eval_episodes))
 
         video_images = [[] for _ in range(eval_episodes)]
+        imagine_images = [[] for _ in range(eval_episodes)]
 
         for i in range(eval_episodes):
             obs = env.reset()
             done = False
             prev_state = self.rssm.init_state(1, self.device)
             prev_action = torch.zeros(1, self.action_size).to(self.device)
-
+            prior = None
             while not done:
                 with torch.no_grad():
                     posterior, action = self.act_with_world_model(obs, prev_state, prev_action)
+                    
+                    if prior is None:
+                        prior = posterior
+                        img_action = action
+                    else:
+                        img_action = self.actor(torch.cat([prior['stoch'], prior['deter']], dim=-1))
+                        prior = self.rssm.imagine_step(prior, img_action)
+                    img_obs = self.obs_decoder(torch.cat([prior['stoch'], prior['deter']], dim=-1)).mean.cpu().numpy()
+                    img_obs = (img_obs[0] + 0.5)*255.0
+                    # print('img_obs shape', img_obs.shape)
+                    # print('obs shape', obs['image'].shape)
+                    imagine_images[i].append(img_obs.copy().clip(0, 255).astype(np.uint8))
+                    
                 action = action[0].cpu().numpy()
                 next_obs, rew, done, _ = env.step(action)
                 prev_state = posterior
@@ -301,9 +316,12 @@ class Dreamer:
                 episode_rew[i] += rew
 
                 if render:
-                    video_images[i].append(obs['image'].transpose(1,2,0).copy())
+                    video_images[i].append(obs['image'].copy())
                 obs = next_obs
-        return episode_rew, np.array(video_images[:self.args.max_videos_to_save])
+            if render:
+                video_images[i] = np.stack(video_images[i])
+                imagine_images[i] = np.stack(imagine_images[i])
+        return episode_rew, video_images, imagine_images
 
     def collect_random_episodes(self, env, seed_steps):
 
@@ -367,21 +385,21 @@ def main():
     parser.add_argument('--seed', type=int, default=1, help='Random seed')
     parser.add_argument('--no-gpu', action='store_true', help="GPUs aren't used if passed true")
     # Data parameters
-    parser.add_argument('--max-episode-length', type=int, default=1000, help='Max episode length')
-    parser.add_argument('--buffer-size', type=int, default=1000000, help='Experience replay size')  # Original implementation has an unlimited buffer size, but 1 million is the max experience collected anyway
-    parser.add_argument('--time-limit', type=int, default=1000, help='time limit') # Environment TimeLimit
+    parser.add_argument('--max-episode-length', type=int, default=2000, help='Max episode length')
+    parser.add_argument('--buffer-size', type=int, default=2000000, help='Experience replay size')  # Original implementation has an unlimited buffer size, but 1 million is the max experience collected anyway
+    parser.add_argument('--time-limit', type=int, default=2000, help='time limit') # Environment TimeLimit
     # Models parameters
     parser.add_argument('--cnn-activation-function', type=str, default='relu', help='Model activation function for a convolution layer')
     parser.add_argument('--dense-activation-function', type=str, default='elu', help='Model activation function a dense layer')
     parser.add_argument('--obs-embed-size', type=int, default=1024, help='Observation embedding size')  # Note that the default encoder for visual observations outputs a 1024D vector; for other embedding sizes an additional fully-connected layer is used
     parser.add_argument('--num-units', type=int, default=400, help='num hidden units for reward/value/discount models')
     parser.add_argument('--deter-size', type=int, default=200, help='GRU hidden size and deterministic belief size')
-    parser.add_argument('--stoch-size', type=int, default=30, help='Stochastic State/latent size')
+    parser.add_argument('--stoch-size', type=int, default=32, help='Stochastic State/latent size')
     # Actor Exploration Parameters
     parser.add_argument('--action-repeat', type=int, default=1, help='Action repeat')
-    parser.add_argument('--action-noise', type=float, default=0.3, help='Action noise')
+    parser.add_argument('--action-noise', type=float, default=0.2, help='Action noise')
     # Training parameters
-    parser.add_argument('--total_steps', type=int, default=5e6, help='total number of training steps')
+    parser.add_argument('--total_steps', type=int, default=1e7, help='total number of training steps')
     parser.add_argument('--seed-steps', type=int, default=5000, help='seed episodes')
     parser.add_argument('--update-steps', type=int, default=100, help='num of train update steps per iter')
     parser.add_argument('--collect-steps', type=int, default=1000, help='actor collect steps per 1 train iter')
@@ -391,22 +409,22 @@ def main():
     parser.add_argument('--use-disc-model', action='store_true', help='whether to use discount model' )
     # Coeffecients and constants
     parser.add_argument('--free-nats', type=float, default=3, help='free nats')
-    parser.add_argument('--discount', type=float, default=0.99, help='discount factor for actor critic')
+    parser.add_argument('--discount', type=float, default=0.995, help='discount factor for actor critic')
     parser.add_argument('--td-lambda', type=float, default=0.95, help='discount rate to compute return')
-    parser.add_argument('--kl-loss-coeff', type=float, default=1.0, help='weightage for kl_loss of model')
+    parser.add_argument('--kl-loss-coeff', type=float, default=0.1, help='weightage for kl_loss of model')
     parser.add_argument('--kl-alpha', type=float, default=0.8, help='kl balancing weight; used for Dreamerv2')
     parser.add_argument('--disc-loss-coeff', type=float, default=10.0, help='weightage of discount model')
     
     # Optimizer Parameters
-    parser.add_argument('--model_learning-rate', type=float, default=6e-4, help='World Model Learning rate') 
-    parser.add_argument('--actor_learning-rate', type=float, default=8e-5, help='Actor Learning rate') 
-    parser.add_argument('--value_learning-rate', type=float, default=8e-5, help='Value Model Learning rate')
-    parser.add_argument('--adam-epsilon', type=float, default=1e-7, help='Adam optimizer epsilon value') 
+    parser.add_argument('--model_learning-rate', type=float, default=2e-4, help='World Model Learning rate') 
+    parser.add_argument('--actor_learning-rate', type=float, default=4e-5, help='Actor Learning rate') 
+    parser.add_argument('--value_learning-rate', type=float, default=1e-4, help='Value Model Learning rate')
+    parser.add_argument('--adam-epsilon', type=float, default=1e-5, help='Adam optimizer epsilon value') 
     parser.add_argument('--grad-clip-norm', type=float, default=100.0, help='Gradient clipping norm')
     # Eval parameters
     parser.add_argument('--test', action='store_true', help='Test only')
-    parser.add_argument('--test-interval', type=int, default=10000, help='Test interval (episodes)')
-    parser.add_argument('--test-episodes', type=int, default=10, help='Number of test episodes')
+    parser.add_argument('--test-interval', type=int, default=20000, help='Test interval (episodes)')
+    parser.add_argument('--test-episodes', type=int, default=20, help='Number of test episodes')
     # saving and checkpoint parameters
     parser.add_argument('--scalar-freq', type=int, default=1e3, help='scalar logging freq')
     parser.add_argument('--log-video-freq', type=int, default=-1, help='video logging frequency')
@@ -440,9 +458,11 @@ def main():
     else:
         device = torch.device('cpu')
 
-    train_env = make_env(args)
-    test_env  = make_env(args)
-    obs_shape = train_env.observation_space['image'].shape
+    # train_env = make_env(args)
+    # test_env  = make_env(args)
+    train_env = Atari(f"ALE/{args.env}-v5", grayscale=False)
+    test_env  = Atari(f"ALE/{args.env}-v5", grayscale=False)
+    obs_shape = train_env.observation_space.shape
     action_size = train_env.action_space.shape[0]
     dreamer = Dreamer(args, obs_shape, action_size, device, args.restore)
 
@@ -493,8 +513,15 @@ def main():
             })
 
             if global_step % args.test_interval == 0:
-                episode_rews, video_images = dreamer.evaluate(test_env, args.test_episodes, render=True)
-
+                episode_rews, video_images, imagine_images = dreamer.evaluate(test_env, args.test_episodes, render=True)
+                
+                wandb.log(
+                    {
+                        'env_acting': [wandb.Video(video_images[i], fps=24) for i in range(4)],
+                        'imagine_acting': [wandb.Video(imagine_images[i], fps=24) for i in range(4)]
+                    },
+                     step=global_step
+                )
                 logs.update({
                     'EVAL/avg_reward':np.mean(episode_rews),
                     'EVAL/max_reward': np.max(episode_rews),
@@ -503,14 +530,8 @@ def main():
                 })
             
             wandb.log(logs, step=global_step)
+            
             logger.log_scalars(logs, global_step)
-
-            if global_step % args.log_video_freq ==0 and args.log_video_freq != -1 and len(video_images[0])!=0:
-                wandb.log(
-                    {'env_acting': [wandb.Video(video_images[i]) for i in range(video_images.shape[0])]},
-                     step=global_step
-                     )
-                logger.log_video(video_images, global_step, args.max_videos_to_save)
 
             if global_step % args.checkpoint_interval == 0:
                 ckpt_dir = os.path.join(logdir, 'ckpts/')
@@ -523,7 +544,7 @@ def main():
 
     elif args.evaluate:
         logs = OrderedDict()
-        episode_rews, video_images = dreamer.evaluate(test_env, args.test_episodes, render=True)
+        episode_rews, video_images, _ = dreamer.evaluate(test_env, args.test_episodes, render=True)
 
         logs.update({
             'TEST/avg_reward':np.mean(episode_rews),
